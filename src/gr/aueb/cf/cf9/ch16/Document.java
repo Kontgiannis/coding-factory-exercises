557d5b64193cb2d35d2ea89895ac85eb80eaa19d
Kontgiannis <ikontog03@gmail.com>
2025-12-24 19:57:59 +0200

    upd: corrected printing message at Document + Photo

--- src/gr/aueb/cf/cf9/ch16/Document.java
+++ src/gr/aueb/cf/cf9/ch16/Document.java
@@ -3,6 +3,6 @@ package gr.aueb.cf.cf9.ch16;
 public class Document implements Printable {
     @Override
     public void print() {
-        System.out.println("This is a Document");
+        System.out.println("Printing Document");
     }
 }
--- src/gr/aueb/cf/cf9/ch16/Photo.java
+++ src/gr/aueb/cf/cf9/ch16/Photo.java
@@ -3,6 +3,6 @@ package gr.aueb.cf.cf9.ch16;
 public class Photo  implements Printable {
     @Override
     public void print() {
-        System.out.println("This is a Photo");
+        System.out.println("Printing Photo");
     }
 }
